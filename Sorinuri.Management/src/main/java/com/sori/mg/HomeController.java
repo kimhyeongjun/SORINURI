b503eb1d3ea7902620b35df84c18ed790ea12280
jun <jun@KING-JUN>
2016-11-01 09:23:59 +0900

    logger 설정

--- Sorinuri.Management/src/main/java/com/sori/mg/HomeController.java
+++ Sorinuri.Management/src/main/java/com/sori/mg/HomeController.java
@@ -2,38 +2,57 @@ package com.sori.mg;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.expression.spel.ast.TypeReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private final String NULL = "";
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		logger.debug("인터셉트 -> " + formattedDate.toString());
+		model.addAttribute("serverTime", formattedDate);
+
 		return "home";
 	}
-	
+
+	@RequestMapping(value = "/ajax", method = RequestMethod.POST)
+	public @ResponseBody String ajax(Model model) {
+		Map<String, String> map = new HashMap<>();
+		try {
+			map.put("id", "id");
+			if (map.get("pw").equals(NULL)) System.out.println("not null");
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e.toString(), e);
+			return "ERROR";
+		}
+		return "SUCCESS";
+	}
+
 }
--- Sorinuri.Management/src/main/webapp/WEB-INF/views/home.jsp
+++ Sorinuri.Management/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,36 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false"%>
 <html>
 <head>
-	<title>Home</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript">
+	function ajaxGo() {
+		jQuery.ajax({
+			type : "POST",
+			url : "ajax",
+			data : {id : "id"},
+			dataType : "text",
+			success : function(obj) {
+				
+					alert(obj);
+			},
+			complete : function(data) {// 응답이 종료되면 실행, 잘 사용하지않는다
+			},
+			error : function(xhr, status, error) {
+				alert("ERROR!!!");
+			}
+		});
+	}
+</script>
+<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
+	<h1>Hello world!</h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+	<P>The time on the server is ${serverTime}.</P>
+	
+	<button onclick="ajaxGo()">AJAX</button>
 </body>
 </html>
